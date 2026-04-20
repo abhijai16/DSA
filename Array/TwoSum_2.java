e7c2ccb441fc3352fd24c6050b5fb888e1e858df
abhijai16 <abhijai123456789@gmail.com>
2026-04-20 22:57:33 +0530

    Corrected the TwoSum_2.java file Optimal Approach by using HashMap.

--- Array/TwoSum_2.java
+++ Array/TwoSum_2.java
@@ -1,23 +1,23 @@
 package Array;
+import java.util.HashMap;
 
-//Good than Brute Force Method with Time Complexity of O(n).
+//Optimal Approach by using HashMap with Time Complexity of O(n).
 
 public class TwoSum_2 {
     public static void main(String[] args) {
         int[] arr = {2, 11, 5, 9, 7, 1, 10, 8, 13, 4, 0};
         int target = 9;
 
-        int i=0;
-        if(arr[i]>target){
-            i++;
-        }
-        else{
-            int search = target-arr[i];
+        HashMap<Integer, Integer> map = new  HashMap<>();
 
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[j]==search){
-                    System.out.println("["+i+","+j+"]");
-                }
+        for(int i = 0; i<arr.length; i++){
+            int complement = target-arr[i];
+
+            if(map.containsKey(complement)){
+                System.out.println("["+map.get(complement)+","+i+"]");
+            }
+            else{
+                map.put(arr[i], i);
             }
         }
     }
