5a69b0780cd5274a3b3696faf33c8130016c83b1
abhijai16 <abhijai123456789@gmail.com>
2026-04-21 23:32:23 +0530

    Updated comments for consistency and expanded the array of problems in RandomDSA.java. Enhanced output formatting for clarity.

--- RandomDSA.java
+++ RandomDSA.java
@@ -1,17 +1,17 @@
 import java.util.Random;
 
-//**  I have created this program for randomly generating Leetcode problems which i can daily practice.
+//**  I have created this program for randomly generating LeetCode problems which i can daily practice.
 
 public class RandomDSA{
     public static void main(String[] args) {
 
-        int[] arr = {1, 189, 268, 1920, 1929, 1480, 1672};
+        int[] arr = {1, 189, 268, 1920, 1929, 1480, 1672, 1470, 1431, 1512};
 
 
-        //**Generating 2 Random Leetcode Problem.
+        //**Generating 2 Random LeetCode Problem.
 
-        System.out.println(getRandom(arr));
-        System.out.println(getRandom(arr));
+        System.out.println("Question 1: " + getRandom(arr));
+        System.out.println("Question 2: " + getRandom(arr));
     }
 
     public static int getRandom(int[] arr) {
