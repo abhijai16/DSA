43b6346e6674d0eb87b9d80a659e862e0fc568f6
abhijai16 <abhijai123456789@gmail.com>
2026-04-23 11:35:39 +0530

    Updated RandomDSA.java to include an additional question in the array and modified comments to reflect the new question generation strategy for future based on difficulty levels.

--- RandomDSA.java
+++ RandomDSA.java
@@ -5,13 +5,20 @@ import java.util.Random;
 public class RandomDSA{
     public static void main(String[] args) {
 
-        int[] arr = {1, 189, 268, 1920, 1929, 1480, 1672, 1470, 1431, 1512, 1295, 1365, 41, 75};
-        //* 14 Questions.
+        int[] arr = {1, 189, 268, 1920, 1929, 1480, 1672, 1470, 1431, 1512, 1295, 1365, 41, 75, 832};
 
-        //**Generating 2 Random LeetCode Problem.
+        //*  After solving some more medium and hard questions, will generate the numbers based on each level.
+        // int[] easy = {1, 268, 1480, 1672, 1470, 1431, 1512, 1295, 1365, 832, 1920, 1929};
+        // int[] medium = {75, 189, 41};
+        // int[] hard = {41};
+
+        //* 15 Questions.
+
+        //**Generating 3 Random LeetCode Problem.
 
         System.out.println("Question 1: " + getRandom(arr));
         System.out.println("Question 2: " + getRandom(arr));
+        System.out.println("Question 3: " + getRandom(arr));
     }
 
     public static int getRandom(int[] arr) {
