e0f8e2ab8244eeb6a5e077312d2637ef94532586
abhijai16 <abhijai123456789@gmail.com>
2026-04-27 20:20:45 +0530

    Implemented Bubble Sort and Selection Sort algorithms with user input for sorting order.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,9 +1,49 @@
 package Sorting;
+import java.util.*;
+
+//* Bubble Sort Time Complexity is O(n^2) and it is a stable Sorting Algorithm.*/
 
 public class BubbleSort {
-   public static void main(String[] args) {
-        int[] arr = {1, 7, 90, 23, 45, 32, 2, 0, 34, 100};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {76, 23, 1, 0, 25, 11, 90, 20};
 
-        
-   }
+        System.out.println("Ascending(a) or Descending(d): ");
+        String type = sc.next();
+        bubbleSort(arr, type);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void  bubbleSort(int[] arr, String type){
+
+        for(int i=0; i<arr.length; i++){
+            boolean swapped = false;
+
+            for(int j=1; j<arr.length-i; j++){
+                if(type.equals("a")){
+                    if(arr[j]<arr[j-1]){
+                        swap(arr, j, j-1);
+                        swapped = true;
+                    }
+                }
+                else if(type.equals("d")){
+                    if(arr[j]>arr[j-1]){
+                        swap(arr, j, j-1);
+                        swapped = true;
+                    }
+                }
+            }
+                //* Array is already Sorted, no need to run the loop again and again. */
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
+
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,52 @@
+package Sorting;
+import java.util.*;
+
+//* Selection Sort Time Complexity is O(n^2) and it is not a stable Sorting Algorithm. */
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {76, 23, 1, 0, 25, 11, 90, 20};
+
+        System.out.println("Ascending(a) or Descending(d): ");
+        String type = sc.next();
+        selectionSort(arr, type);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr, String type){
+        if(type.equals("a")){
+            for(int i=0; i<arr.length; i++){
+                int last = arr.length-i-1;
+                int max = getMaxIndex(arr, 0, last);
+                swap(arr, max, last);
+            }
+        }
+        else if(type.equals("d")){
+           for(int i=0; i<arr.length; i++){
+                int first = i;
+                int max = getMaxIndex(arr, first, arr.length-1);
+                swap(arr, max, first);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    //* For Ascending order. */
+    public static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+
+        for(int i=start; i<=end; i++){
+            if(arr[i]>arr[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+}
