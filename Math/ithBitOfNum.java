514491ec987cef1f3860d5fcd9a6625800d0f1d2
abhijai16 <abhijai123456789@gmail.com>
2026-05-10 21:32:07 +0530

    Add new classes for finding the ith bit of a number and calculating a magic number in their respective files.

---
+++ Math/ithBitOfNum.java
@@ -0,0 +1,19 @@
+package Math;
+
+//* Find the ith bit of a number. */
+
+public class ithBitOfNum {
+    public static void main(String[] args) {
+        int a = 9; //1001
+        int n = 3;
+
+        int ans = a&(1<<n-1);   // for n=3, 1001 & 0100 => 0000   or  for n=4, 1001 & 1000 => 1000 
+
+        if(ans==0){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("1");
+        }
+    }
+}
