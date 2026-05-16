75668000693c91fbfec09e1af3a6782094d080c0
abhijai16 <abhijai123456789@gmail.com>
2026-05-16 21:53:22 +0530

    Update LCM calculation in LCM.java to use a more efficient formula and change the second input value from 12 to 28.

--- Math/LCM.java
+++ Math/LCM.java
@@ -3,11 +3,11 @@ package Math;
 public class LCM {
     public static void main(String[] args) {
         int a = 14;
-        int b = 12;
+        int b = 28;
 
         int d = gcd(a, b);
 
-        System.out.println("LCM : " + ((a/d)*(b/d)*d));
+        System.out.println("LCM : " + (a*b)/d);
     }
 
     public static int gcd(int a , int b){
