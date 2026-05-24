5d2697057d1e379788081010d3f3dce0bb107524
abhijai16 <abhijai123456789@gmail.com>
2026-05-24 19:43:57 +0530

    Add subsetWithDuplicates method to ArraySubset.java for generating unique subsets from an array containing duplicates. Update main method to demonstrate this new functionality.

--- Array/ArraySubset.java
+++ Array/ArraySubset.java
@@ -3,16 +3,19 @@ package Array;
 //* Time Complexity of O(N*2^N) */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArraySubset{
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+        int[] arr = {1, 2, 2};
 
-        List<List<Integer>> list = subset(arr);
+        List<List<Integer>> list = subsetWithDuplicates(arr);
         System.out.println(list);
     }
 
+
+    //* If the array contains duplicate elements like [1, 2, 2] then it will create subset of [1,2] twice with this approach. */
     public static List<List<Integer>> subset(int[] arr){
         List<List<Integer>> outer = new ArrayList<>();
 
@@ -28,6 +31,34 @@ public class ArraySubset{
             }
         }
 
+        return outer;
+    }
+
+    //* This will not contain duplicate subsets. */
+    public static List<List<Integer>> subsetWithDuplicates(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        int start = 0, end = 0;
+        for(int i=0; i<arr.length; i++){
+            start = 0;
+
+            if(i>0 && arr[i]==arr[i-1]){
+                start = end + 1;
+            }
+            
+            end = outer.size()-1;
+            int size = outer.size();
+
+            for(int j=start; j<size; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
         return outer;
     }
 }
