266a048cad64bc6fc5557d1a98e5b5cb43d319ac
abhijai16 <abhijai123456789@gmail.com>
2026-05-26 21:39:58 +0530

    Update RandomDSA.java to increase the easy and medium question counts, add new problems to the arrays, and include a fourth question from the hard category. This enhances the variety of problems available for random selection.

--- RandomDSA.java
+++ RandomDSA.java
@@ -5,17 +5,18 @@ import java.util.Random;
 public class RandomDSA{
     public static void main(String[] args) {
 
-        int[] easy = {1, 268, 1480, 1672, 1470, 1431, 1512, 1295, 1365, 832, 1920, 1929, 744, 704, 867, 448, 645, 136, 191, 1342, 3658};
-        int[] medium = {75, 189, 34, 852, 162, 33, 1011, 2226, 287, 442, 238, 17};
-        int[] hard = {41, 1095, 410, 1250, 37};
+        int[] easy = {1, 268, 1480, 1672, 1470, 1431, 1512, 1295, 1365, 832, 1920, 1929, 744, 704, 867, 448, 645, 136, 191, 1342, 3658, 1389, 1832, 1732, 66, 26};
+        int[] medium = {75, 189, 34, 852, 162, 33, 1011, 2226, 287, 442, 238, 17, 1985, 167, 15, 16};
+        int[] hard = {41, 1095, 410, 1250, 37, 42};
 
-        //* 38 Questions.
+        //* 48 Questions.
 
         //* Generating 3 Random LeetCode Problem.
 
         System.out.println("Question 1: " + getRandom(easy));
         System.out.println("Question 2: " + getRandom(easy));
         System.out.println("Question 3: " + getRandom(medium));
+        System.out.println("Question 4: " + getRandom(hard));
     }
 
     public static int getRandom(int[] arr) {
